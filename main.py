ade2fbd18b0b5880cf8bdc6b68e1b114ccd746e7
James <61007893+distortedjams@users.noreply.github.com>
2023-01-24 11:06:27 -0500

    Update main.py
    updated version adding functions

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+#Import file where we are going to store our funcitons
+#import functions.py
+def aes_encrypt():
+     print("AES ENCRYPT") #DONT USE AES 
+
+def des_encrypt():
+    print("DES ENCRYPT")
+    
+def des3_encrypt():
+    print("3DES ENCRYPT")
+
+def aes_decrypt():
+    print("AES DECRYPT") #DONT USE AES 
+    
+def des_decrypt():
+    print("DES DECRYPT")
+    
+def des3_decrypt():
+    print("3DES DECRYPT")
+    
+
 print("Welcome to the main center of communication")
 while True:
     fInput = int(input('Please select a process 1: Encryption, 2: Decryption'))
@@ -5,24 +26,24 @@ while True:
         #This is where the encryption portion goes
         sInput = int(input('Please select a encryption method 1.AES? 2. DES 3. 3DES'))
         if sInput == 1:
-                print("AES ENCRYPT")
-            elif sInput == 2:
-                print("DES ENCRYPT")
-            elif sInput == 3:
-                print("3DES ENCRYPT")
-            else
-                print("ERROR: Incorrect input please try again")
+                aes_encrypt()
+        elif sInput == 2:
+                des_encrypt()
+        elif sInput == 3:
+                des3_encrypt()
+        else:
+            print("ERROR: Incorrect input please try again")
     elif fInput == 2:
         #This is where the decryption portion goes
         sInput = int(input('Please select a decryption method 1.AES? 2. DES 3. 3DES'))
-            if sInput == 1:
-                print("AES DECRYPT")
-            elif sInput == 2:
-                print("DES DECRYPT")
-            elif sInput == 3:
-                print("3DES DECRYPT")
-            else
-                print("ERROR: Incorrect input please try again")
+        if sInput == 1:
+            aes_decrypt()
+        elif sInput == 2:
+            des_decrypt()
+        elif sInput == 3:
+            des3_decrypt()
+        else:
+            print("ERROR: Incorrect input please try again")
     else:
         #User inputted a weird input make them do it again
         print("ERROR: Incorrect input please try again")
