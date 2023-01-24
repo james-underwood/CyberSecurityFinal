2b2d3936503c8a6a5bad30239a70b9aa30fa3456
James <61007893+distortedjams@users.noreply.github.com>
2023-01-24 13:52:58 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,25 @@
+def aes_encrypt():
+     print("AES ENCRYPT") #DONT USE AES 
+
+
+
+def des_encrypt():
+    print("DES ENCRYPT")
+
+    
+
+def des3_encrypt():
+    print("3DES ENCRYPT")
+
+
+
+def aes_decrypt():
+    print("AES DECRYPT") #DONT USE AES 
+
+def des_decrypt():
+    print("DES DECRYPT")
+
+    
+
+def des3_decrypt():
+    print("3DES DECRYPT")
